afe8f7c90a3e90923bbb68c22e849256bd5c2c21
Глеб Капустин <gleb.kapustin.98@mail.ru>
2025-05-16 18:36:18 +0300

    Initial Commit

---
+++ Weather1221/ContentView.swift
@@ -0,0 +1,24 @@
+//
+//  ContentView.swift
+//  Weather1221
+//
+//  Created by Глеб Капустин on 16.05.2025.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        VStack {
+            Image(systemName: "globe")
+                .imageScale(.large)
+                .foregroundStyle(.tint)
+            Text("Hello, world!")
+        }
+        .padding()
+    }
+}
+
+#Preview {
+    ContentView()
+}
---
+++ Weather1221/Weather1221App.swift
@@ -0,0 +1,17 @@
+//
+//  Weather1221App.swift
+//  Weather1221
+//
+//  Created by Глеб Капустин on 16.05.2025.
+//
+
+import SwiftUI
+
+@main
+struct Weather1221App: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
